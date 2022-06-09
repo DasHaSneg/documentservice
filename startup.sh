c8702642d94446bd3f14ca66874ed0fb64d6fa3d
Daria <dashasneg@mail.ru>
2022-06-10 02:48:03 +0500

    fix docker file

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ CHAIN_ID=test
 
 if [ -f ~/.documentservice/config/genesis.json ]; then
     echo "run node"
-    documentserviced start --home ~/.documentservice
+    /usr/local/bin/documentserviced start --home ~/.documentservice
 else
 	echo "Config empty"
 fi
